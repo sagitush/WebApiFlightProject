490ae3d5ecfb6d262fcc4a8c5d9e6be725f6ddd3
sagitush <44786027+sagitush@users.noreply.github.com>
2019-12-05 19:00:21 +0200

    Add files via upload

---
+++ Controllers/BasicAuthenticationAdministratorAttribute.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Principal;
+using System.Text;
+using System.Web;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+using FlightCenterProject;
+
+
+namespace FlightWebApplication.Controllers
+{
+    public class BasicAuthenticationAdministratorAttribute: AuthorizationFilterAttribute
+    {
+        public override void OnAuthorization(HttpActionContext actionContext)
+        {
+            //Checks whether user information has been entered
+            if (actionContext.Request.Headers.Authorization == null)
+            {
+                actionContext.Response = actionContext.Request.CreateResponse(HttpStatusCode.Forbidden,
+                    "You must send user name and password in basic authentication");
+                return;
+            }
+
+            //Encoding the user name and password from base64
+            string authenticationToken = actionContext.Request.Headers.Authorization.Parameter;
+            string decodedAuthenticationToken = Encoding.UTF8.GetString(
+                Convert.FromBase64String(authenticationToken));
+
+            string[] usernamePasswordArray = decodedAuthenticationToken.Split(':');
+            string username = usernamePasswordArray[0];
+            string password = usernamePasswordArray[1];
+
+            if (username == "admin" && password == "9999")
+            {               
+                FacadeBase fb = FlyingCenterSystem.GetInstance().Login(username, password, out LoginTokenBase token);
+                actionContext.Request.Properties["administratorToken"] = token;
+                LoggedInAdministratorFacade administratorFacade = (LoggedInAdministratorFacade)fb;
+                actionContext.Request.Properties["administratorFacade"] = administratorFacade;
+            }
+            else
+            {              
+                actionContext.Response = actionContext.Request.CreateResponse(HttpStatusCode.Unauthorized,
+                    "You are not authorized");
+            }
+        }
+    }
+}
